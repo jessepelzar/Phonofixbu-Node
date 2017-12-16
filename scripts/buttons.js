e0057685fb94be9f5d2120628fcd358c8787d26a
jessepelzar <jessepelzar@gmail.com>
2017-12-15 22:21:18 -0500

    init

---
+++ scripts/buttons.js
@@ -0,0 +1,9 @@
+let scrollAction = function(elementId) {
+  let minPixels = 16;
+  let maxPixels = 30;
+  let targetLoc = document.getElementById('issue_select');
+  let scrollContainer = targetLoc;
+}
+let startButton = document.getElementById('start_button');
+
+startButton.addEventListener("click", )
