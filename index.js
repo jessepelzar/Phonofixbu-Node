5a6388274aa1334a4836cbda96653445935a7fcc
jessepelzar <jessepelzar@gmail.com>
2018-08-03 22:39:20 -0400

    attempt

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+var PORT = process.env.PORT || 5050
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
