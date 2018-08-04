79ff9bad21cfff92c833ab672a43093b5a427390
jessepelzar <jessepelzar@gmail.com>
2018-08-03 21:34:12 -0400

    first commit

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
 
 
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const router = express.Router();
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-const PORT = process.env.PORT || 5000;
-var AWS = require('aws-sdk');
-var app = express();
-
-app.use(express.static(path.join(__dirname, 'src')));
-// parse html forms
-app.use(bodyParser.urlencoded({ extended : false }));
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.get('/', (req, res) => res.render('pages/index.ejs'));
-
-
-
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
